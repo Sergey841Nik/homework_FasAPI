e33fba695b2942c383125cd3fa085c107b6a8539
Сергей <nikitos841@yandex.ru>
2025-01-21 21:52:22 +0400

    change_module_16_1

--- module_16_1.py
+++ module_16_1.py
@@ -7,22 +7,22 @@ app = FastAPI()
 
 @app.get("/")
 def home_page():
-    return {"message": "Главная страница"}
+    return "Главная страница"
 
 @app.get("/user/admin")
 def admin_page():
-    return {"message": "Вы вошли как админ"}
+    return "Вы вошли как админ"
 
 @app.get("/user/{user_id}")
 def user_page(user_id: int):
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
+    return f"Вы вошли как пользователь № {user_id}"
 
 @app.get("/user")
 def user_info(
     username: str,
     age: int,
 ):
-    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
 
 
 if __name__ == "__main__":
